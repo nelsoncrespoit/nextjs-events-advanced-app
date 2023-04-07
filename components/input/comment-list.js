9d2e17c51b597f6023e973a20d1de4b2075363d4
nelsoncrespoit <nelsoncrespo1954@gmail.com>
2023-04-07 02:08:18 +0100

    First Commit

---
+++ components/input/comment-list.js
@@ -0,0 +1,43 @@
+import classes from './comment-list.module.css';
+
+function CommentList(props) {
+  const { items } = props;
+
+  return (
+    <ul className={classes.comments}>
+      {items.map((item) => (
+        <li key={item._id}>
+          <p>{item.text}</p>
+          <div>
+            By <address>{item.name}</address>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CommentList;
+
+
+// import classes from "./comment-list.module.css";
+
+// function CommentList(props) {
+//   const { items } = props;
+
+//   return (
+//     <ul className={classes.comments}>
+//       {/* Render list of comments - fetched from API */}
+//       {items.map((item) => (
+//         <li key={item._id}>
+//           <p>{item.text}</p>
+//           <div>
+//             By <address>{item.name}</address>
+//           </div>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default CommentList;
